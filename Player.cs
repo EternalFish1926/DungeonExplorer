fa1b14c7277b994b89c8c3afcb3f98e8c3fd640b
EternalFish1926 <matty103a@googlemail.com>
2025-03-13 11:41:55 +0000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
 namespace DungeonExplorer
 {
     public class Player
     {
         public string Name { get; private set; }
-        public int Health { get; private set; }
+        public int Health { get; set; }
         private List<string> inventory = new List<string>();
 
         public Player(string name, int health)
@@ -16,12 +17,19 @@ namespace DungeonExplorer
 
         public void PickUpItem(string item)
         {
+            //adds item to inventory
             inventory.Add(item);
         }
-
+    
         public string InventoryContents()
         {
+            // inventory contents displays empty if the player has not picked up anything
             return inventory.Count > 0 ? string.Join(", ", inventory) : "empty";
+        }
+
+        internal void PickUpItem(object item)
+        {
+            throw new NotImplementedException();
         }
     }
 }
