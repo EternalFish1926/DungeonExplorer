ccaa1619dbb71ae2cac7a663683aa9ce09c60f42
Jacob <eternalfish23@gmail.com>
2025-03-13 11:20:36 +0000

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -17,6 +17,7 @@ namespace DungeonExplorer
 
         public void Start()
         {
+            //Introduction to room
             Console.WriteLine("Welcome to Dungeon Explorer!");
             Console.WriteLine($"You find yourself in: {currentRoom.GetDescription()}");
 
@@ -24,11 +25,13 @@ namespace DungeonExplorer
 
             while (playing)
             {
+                //Asks the player for input
                 Console.WriteLine("\nWhat do you want to do? (look/move/take/inventory/quit)");
                 string input = Console.ReadLine().ToLower();
 
                 switch (input)
                 {
+                    //Gets description of room and items inside the room 
                     case "look":
                         Console.WriteLine($"You look around: {currentRoom.GetDescription()}");
                         if (currentRoom.Item != null)
@@ -36,12 +39,12 @@ namespace DungeonExplorer
                             Console.WriteLine($"You see a {currentRoom.Item} on the ground.");
                         }
                         break;
-
+                        //Moves through the room 
                     case "move":
                         Console.WriteLine("You move deeper into the dungeon...");
 
                         break;
-
+                        //Picks up item(s) in the room if any are seen  
                     case "take":
                         if (currentRoom.Item != null)
                         {
@@ -54,16 +57,16 @@ namespace DungeonExplorer
                             Console.WriteLine("There's nothing to take.");
                         }
                         break;
-
+                        //Displays the players inventory
                     case "inventory":
                         Console.WriteLine("Your inventory: " + player.InventoryContents());
                         break;
-
+                        //Quits the game
                     case "quit":
                         playing = false;
                         Console.WriteLine("Thanks for playing!");
                         break;
-
+                        //Any other commands show as invalid
                     default:
                         Console.WriteLine("Invalid command. Try again.");
                         break;
@@ -71,5 +74,4 @@ namespace DungeonExplorer
             }
         }
     }
-
 }
