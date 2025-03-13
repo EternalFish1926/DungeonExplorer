80bf1eae58b12b7ad92a4bb628b76e1dc4160bc2
Jacob <eternalfish23@gmail.com>
2025-03-13 11:21:38 +0000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -17,11 +17,13 @@ namespace DungeonExplorer
 
         public void PickUpItem(string item)
         {
+            //adds item to inventory
             inventory.Add(item);
         }
-
+    
         public string InventoryContents()
         {
+            // inventory contents displays empty if the player has not picked up anything
             return inventory.Count > 0 ? string.Join(", ", inventory) : "empty";
         }
 
